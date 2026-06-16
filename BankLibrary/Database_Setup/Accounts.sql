a9583ca21181c5936bb88cd770efc3e0f04f0618
Matej Pukancik <matejpukan@gmail.com>
2026-06-17 01:53:25 +0200

    Add Accounts table SQL script and Database_Setup folder

---
+++ BankLibrary/Database_Setup/Accounts.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Accounts] (
+    [AccountNumber] NVARCHAR (20)   NOT NULL,
+    [FirstName]     NVARCHAR (50)   NOT NULL,
+    [LastName]      NVARCHAR (50)   NOT NULL,
+    [Balance]       DECIMAL (29, 2) NOT NULL,
+    [AccountType]   CHAR (1)        NOT NULL,
+    PRIMARY KEY CLUSTERED ([AccountNumber] ASC)
+);
+
